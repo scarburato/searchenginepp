2a9806e2be29b53677f14d3352bd5f7939d25f8b
Froffri <fra.londretti@gmail.com>
2023-10-21 20:30:23 +0200

    Created IndexBuilder class and implemented add method

---
+++ src/indexBuilder/IndexBuilder.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "IndexBuilder.hpp"
+
+namespace index 
+{
+
+void IndexBuilder::write_to_disk(std::ostream &inverted_indices_teletype, std::ostream &lexicon_teletype)
+{
+
+}
+
+}
+
---
+++ src/indexBuilder/IndexBuilder.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+#include "../index/types.hpp"
+
+namespace index
+{
+
+class IndexBuilder
+{
+    private:
+
+    std::map<std::string, std::vector<std::pair<docid_t, freq_t>>> term_to_inverted_index;
+
+    public:
+
+    /*
+    * Function that adds a frequency of a term associated to a documentID
+    * It is assumed to recieve strictly increasing docIDs 
+    */
+    void add(std::string term, docid_t id, freq_t occurences)
+    {
+        // If the term is not in the index I create the vector
+        if(term_to_inverted_index.find(term) == term_to_inverted_index.end())
+        {
+            term_to_inverted_index[term] = std::vector<std::pair<docid_t, freq_t>>();
+        }
+
+        term_to_inverted_index[term].push_back(std::make_pair(id, occurences));
+
+    }
+
+    void write_to_disk(std::ostream& inverted_indices_teletype, std::ostream& lexicon_teletype);
+};
+
+}
