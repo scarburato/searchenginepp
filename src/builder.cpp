62bdfdfd04316c88eb6265e37aed290722c9b434
ricky <rickymarinsalda@hotmail.com>
2023-10-23 10:49:48 +0200

    Implemneted Index and reder
    - Partially implemented Index class' constructor
    - Implemented function to read lines

--- src/builder.cpp
+++ src/builder.cpp
@@ -1,5 +1,63 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include "index/types.hpp"
+
+using namespace index;
+
+const int CHUNK_SIZE = 3; // number of documents to read
+/*
+ * The uncompressed file contains one document per line.
+ * Each line has the following format:
+ * <pid>\t<text>\n
+ * where <pid> is the docno and <text> is the document content
+ */
+void process_chunk(std::unique_ptr<std::vector<std::string>> chunk, docid_t base_id) {
+    for (const auto &line : *chunk) {
+        std::istringstream iss(line);
+        std::string pid, text;
+        if (std::getline(iss, pid, '\t') && std::getline(iss, text, '\n')) {
+            docno_t docno = std::stoul(pid);
+            // esempio stampa docno e text
+            std::cout << "docno: " << docno << ", text: " << text << std::endl;
+        }
+    }
+}
+
 
 int main()
 {
-	return 0;
+    const int CHUNK_SIZE = 10; // example for chunk size
+    std::ifstream file("collection.tsv");
+    if (!file) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    docid_t base_id = 0; // Initialize base_id
+    int line_count = 0;
+    std::unique_ptr<std::vector<std::string>> chunk(new std::vector<std::string>());
+
+    while (std::getline(file, line)) {
+        chunk->push_back(line);
+        line_count++;
+
+        if (line_count == CHUNK_SIZE) {
+            process_chunk(std::move(chunk), base_id);
+            base_id += line_count;
+            line_count = 0;
+            chunk.reset(new std::vector<std::string>());
+        }
+    }
+
+    // Process the remaining lines which are less than CHUNK_SIZE
+    if (!chunk->empty()) {
+        process_chunk(std::move(chunk), base_id);
+    }
+
+    file.close();
+    return 0;
 }
--- src/index/types.hpp
+++ src/index/types.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <map>
 #include "../codes/variable_blocks.hpp"
 
 namespace index
@@ -14,4 +15,29 @@ using IndexDecoder = codes::VariableBlocksDecoder<EncondedDataIterator, docid_t>
 template<typename RawDataIterator>
 using IndexEncoder = codes::VariableBlocksEncoder<RawDataIterator>;
 
+class Index{
+private:
+    std::map<std::string, std::pair<std::streampos, std::streampos>> term_to_inverted_index;
+    std::unique_ptr<std::istream> inverted_indices;
+public:
+    Index(const std::string& map_filename, const std::string& inverted_indices_filename){
+        // Open the file streams
+        std::ifstream map_file(map_filename, std::ios::binary);
+        std::ifstream inverted_indices_file(inverted_indices_filename, std::ios::binary);
+
+        if (!map_file.is_open() || !inverted_indices_file.is_open()) {
+            throw std::runtime_error("Failed to open file");
+        }
+
+        // Read data from the map file using IndexDecoder
+        IndexDecoder<std::istreambuf_iterator<char>> index_decoder(map_file, std::istreambuf_iterator<char>());
+        // Perform operations to fill term_to_inverted_index using index_decoder
+
+        // Save the inverted indices file stream
+        inverted_indices = std::make_unique<std::ifstream>(std::move(inverted_indices_file));
+
+        // Close the file streams
+        map_file.close();
+    }
+};
 }
