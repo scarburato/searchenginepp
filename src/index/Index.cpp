85067251d7a32f7be3faa835e6a0ac4d05930616
dario <dariopag45@gmail.com>
2023-10-23 17:26:07 +0200

    fixed ricky's mess
    - Now builder reads from std::cin
    - updated gitignore
    - Index properly maps memory now!

---
+++ src/index/Index.cpp
@@ -0,0 +1,46 @@
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "Index.hpp"
+
+namespace index
+{
+
+/**
+ * This procedure mmaps a file in memory
+ * @param filename
+ * @return
+ */
+static std::pair<void*, size_t> mmap_helper(char const *const filename)
+{
+	auto fd = open(filename, O_RDONLY);
+
+	// get file's size
+	struct stat st = {0};
+	auto ret_stat= stat(filename, &st);
+
+	if(ret_stat == -1)
+		abort();
+
+	// map file in memory
+	void* mapped = (uint8_t *)mmap(nullptr, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+
+	close(fd);
+
+	return {mapped, st.st_size};
+}
+
+Index::Index(const std::string &map_filename, const std::string &inverted_indices_filename)
+{
+	auto inverted_indices_minfo = mmap_helper(inverted_indices_filename.c_str());
+	inverted_indices = (uint8_t*)inverted_indices_minfo.first;
+	inverted_indices_length = inverted_indices_minfo.second;
+}
+
+Index::~Index()
+{
+	munmap(inverted_indices, inverted_indices_length);
+}
+
+}
---
+++ src/index/Index.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstdint>
+#include <map>
+#include <vector>
+#include "../codes/variable_blocks.hpp"
+#include "types.hpp"
+
+namespace index
+{
+
+struct DocumentInfo
+{
+	docno_t docno;
+	uint32_t lenght;
+};
+
+/**
+ * This class represents the index and is used to access the documents'
+ * information regarding the terms they contain and their frequencies.
+ */
+class Index{
+private:
+	std::map<std::string, std::pair<size_t, size_t>> term_to_inverted_index;
+
+	uint8_t *inverted_indices;
+	size_t inverted_indices_length;
+
+	uint8_t *inverted_indices_freqs;
+	size_t inverted_indices_freqs_length;
+
+	std::vector<DocumentInfo> document_index;
+
+public:
+	/**
+	 * Create an Index from files
+	 * @param map_filename
+	 * @param inverted_indices_filename
+	 */
+	Index(const std::string& map_filename, const std::string& inverted_indices_filename);
+	~Index();
+};
+
+}
