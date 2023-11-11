8a4adea4d0303fbefd98fb53117c81a556acc24a
rickymarinsalda <rickymarinsalda@hotmail.com>
2023-11-11 08:49:51 +0100

    Init reader. it's a test

---
+++ src/codes/diskmap/reader.hpp
@@ -0,0 +1,23 @@
+//
+// Created by ricky on 10/11/23.
+//
+#pragma once
+#include <string>
+
+
+namespace codes{
+
+template<class Value, size_t B = 512>
+class disk_map{
+
+public:
+
+	disk_map(const std::string &file_name){
+
+	}
+	~disk_map(){
+
+	}
+};
+
+}
