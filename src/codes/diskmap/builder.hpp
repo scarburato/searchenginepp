7ee3adebb60d06e7f768f288c04c44e2ee2c5bb7
Froffri <fra.londretti@gmail.com>
2023-11-13 16:22:35 +0100

    Implemented the multitype meta-programming for Value
    Inverted the writing of the postfix and the values
    
    Br0

--- src/codes/diskmap/builder.hpp
+++ src/codes/diskmap/builder.hpp
@@ -1,10 +1,15 @@
 #pragma once
+#include <array>
 #include <cstddef>
 #include <cstdint>
+#include <numeric>
 #include <ostream>
 #include <sys/types.h>
+#include <type_traits>
+#include <utility>
 #include <vector>
 #include "../variable_blocks.hpp"
+#include "../../meta.hpp"
 
 constexpr size_t BLOCK_SIZE = 1024;
 
@@ -18,6 +23,17 @@ private:
     size_t current_bytes = 0;
     size_t i = 0;
     size_t n_strings = 0;
+
+    static constexpr size_t N = [] {
+		if constexpr (std::is_integral_v<Value>)
+			return 1;
+
+		if constexpr (is_std_array_v<Value>)
+			return Value().size();
+
+
+		return Value::serialize_size;
+	}();
 
     static size_t compute_common_prefix(const char *a, const char *b)
     {
@@ -45,7 +61,19 @@ public:
 
     void add(const std::string& key, Value value)
     {
-        auto cValues = codes::VariableBytes(value);
+        // auto cValues = codes::VariableBytes(value);
+
+        std::array<codes::VariableBytes, N> cValues;
+        if constexpr (std::is_integral_v<Value> or std::is_array_v<Value>)
+            for(auto val : value)
+                cValues.push_back(codes::VariableBytes(val));
+        else // Object
+            for(auto val : value.serialize())
+                cValues.push_back(codes::VariableBytes(val));
+
+        // Sum of total used bytes of all the values
+        size_t total_used_bytes = std::accumulate(cValues.begin(), cValues.end(), 0, [](size_t sum, const auto& cValue) { return sum + cValue.used_bytes; });
+        
         n_strings += 1;
 
         if(heads.empty())
@@ -53,9 +81,11 @@ public:
             heads.push_back(key);
 
             teletype.put(0);
-            teletype.write((char*)cValues.bytes, sizeof(cValues.bytes));
+            for(auto cValue : cValues)
+                teletype.write((char*)cValue.bytes, cValue.used_bytes);
 
-            current_bytes = 1 + sizeof(cValues);
+            current_bytes = 1 + total_used_bytes;
+            
             i += 1;
             return;
         }
@@ -64,7 +94,7 @@ public:
         uint8_t common_len = compute_common_prefix(key.c_str(), heads.back().c_str());
         size_t diff_len = key_len - common_len;
 
-        if(current_bytes + sizeof(common_len) + diff_len + sizeof(cValues)> BLOCK_SIZE)
+        if(current_bytes + sizeof(common_len) + diff_len + total_used_bytes> BLOCK_SIZE)
         {
             heads.push_back(key);
             align_stream_to_block(teletype);
@@ -72,17 +102,19 @@ public:
             auto ci = codes::VariableBytes(i++);
 
             teletype.write((char*)ci.bytes, ci.used_bytes);
-            teletype.write((char*)cValues.bytes, cValues.used_bytes);
+            for(auto cValue : cValues)
+                teletype.write((char*)cValue.bytes, cValue.used_bytes);
 
-            current_bytes = ci.used_bytes + sizeof(cValues);
+            current_bytes = ci.used_bytes + total_used_bytes;
             return;
         }
 
         teletype.write((char*)&common_len, sizeof(common_len));
-        teletype.write((char*)cValues.bytes, cValues.used_bytes);
         teletype.write(key.c_str() + common_len, diff_len);
+        for(auto cValue : cValues)
+            teletype.write((char*)cValue.bytes, cValue.used_bytes);
 
-        current_bytes += sizeof(common_len) + diff_len + sizeof(cValues);
+        current_bytes += sizeof(common_len) + diff_len + total_used_bytes;
     }
 
     void finalize()
