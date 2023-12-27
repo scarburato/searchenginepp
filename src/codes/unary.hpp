56eb9281212079b29d2635d730fae671a8af90e7
rickymarinsalda <rickymarinsalda@hotmail.com>
2023-12-27 19:25:31 +0100

    Added comments to make our code more readble ( unary and variable blocks)

--- src/codes/unary.hpp
+++ src/codes/unary.hpp
@@ -5,7 +5,12 @@ namespace codes
 {
 
 /**
- * Serialize an offset and a bit offset in a single uint64_t
+ * Function to serialize an offset and a bit offset into a single uint64_t.
+ * This function 'serialize_bit_offset' combines an 'offset' and a 'bit_offset' into a single uint64_t value.
+ * It ensures that the 'bit_offset' value is less than 8, allowing it to fit within a single byte (8 bits).
+ * The combination is achieved by left-shifting the 'offset' bits by 3 positions (effectively multiplying 'offset' by 8)
+ * and then using bitwise OR to merge these shifted bits with the 'bit_offset'.
+ * The resulting uint64_t value represents the serialized combination of 'offset' and 'bit_offset'.
  * @param offset
  * @param bit_offset
  * @return
@@ -17,7 +22,18 @@ inline uint64_t serialize_bit_offset(size_t offset, unsigned bit_offset)
 }
 
 /**
- * Deserialize an offset and a bit offset from a single uint64_t
+ * Function to deserialize an offset and a bit offset from a single uint64_t.
+ * The function 'deserialize_bit_offset' reverses the process of serialization,
+ * extracting the original 'offset' and 'bit_offset' values from a serialized uint64_t value.
+ * It takes a single uint64_t 'serialized' value as input.
+ *
+ * To obtain the 'offset', it performs a right shift by 3 positions on the 'serialized' value,
+ * undoing the left shift performed during serialization (essentially dividing 'offset' by 8).
+ *
+ * For the 'bit_offset', the function uses a bitwise AND operation with 0b111 (binary 111),
+ * preserving the rightmost 3 bits, which represents the 'bit_offset' in the serialized value.
+ *
+ * The extracted 'offset' and 'bit_offset' are returned as a std::pair<size_t, unsigned>.
  * @param offset
  * @param bit_offset
  * @return
@@ -26,7 +42,7 @@ inline std::pair<size_t, unsigned> deserialize_bit_offset(uint64_t serialized)
 {
 	return {serialized >> 3, serialized & 0b111};
 }
-
+// UnaryDecoder class for decoding encoded unary data
 template<typename EncondedDataIterator>
 class UnaryDecoder
 {
@@ -35,6 +51,7 @@ class UnaryDecoder
 	unsigned start_off = 0;
 public:
 	/**
+	 * Constructor for UnaryDecoder
 	 * 0 will be decoded to 1, as a side effect, padding bits in the last byte will be decoded as 1s!
 	 *
 	 * @param start Where to start reading
@@ -45,7 +62,7 @@ public:
 	{
 		assert(start_off < 8);
 	}
-
+	// Iterator class within UnaryDecoder for decoding unary-encoded data
 	class iterator
 	{
 	public:
@@ -55,12 +72,23 @@ public:
 		using pointer = uint64_t*;  // or also value_type*
 		using reference = uint64_t&;  // or also value_type&
 	private:
-		EncondedDataIterator current_encoded_it;
-		EncondedDataIterator end_encoded_it;
-		uint64_t current_datum_decoded = 0;
-		uint8_t bit_mask = 0b00000001;
-		uint8_t current_datum_start_bit_mask = 0b00000001;
+		// Iterator member variables
+		EncondedDataIterator current_encoded_it; // Iterator pointing to current position in encoded data
+		EncondedDataIterator end_encoded_it; // Iterator pointing to the end of encoded data
+		uint64_t current_datum_decoded = 0; // Current decoded unary value
+		uint8_t bit_mask = 0b00000001; // Bit mask for selecting bits in a byte
+		uint8_t current_datum_start_bit_mask = 0b00000001;  // Bit mask for the start of current datum
+		// Iterator constructor
 
+		/**
+		 * Explicit constructor for the iterator class.
+		 * Initializes the iterator object with iterators pointing to the current and end positions in the encoded data,
+		 * along with an optional bit offset (defaulted to 0).
+		 *
+		 * @param pos Iterator pointing to the current position in the encoded data
+		 * @param end Iterator pointing to the end of the encoded data
+		 * @param bit_off Bit offset value (optional, default: 0)
+		 */
 		explicit iterator(EncondedDataIterator pos, EncondedDataIterator end, unsigned bit_off = 0):
 			current_encoded_it(pos), end_encoded_it(end), bit_mask(0b00000001u << bit_off),
 			current_datum_start_bit_mask(0b00000001u << bit_off) {}
@@ -73,61 +101,84 @@ public:
 		{
 			if (bit_mask == 0b10000000)
 			{
-				bit_mask = 0b00000001;
+				bit_mask = 0b00000001; // If end of byte reached, reset bit mask and move to next byte
 				++current_encoded_it;
 				return;
 			}
 
-			bit_mask = bit_mask << 1;
+			bit_mask = bit_mask << 1; // Move to the next bit in the current byte
 		};
-
+		// Function to parse the current bits and decode the unary value
 		void parse_current()
 		{
-			current_datum_start_bit_mask = bit_mask;
-			current_datum_decoded = 1;
+			current_datum_start_bit_mask = bit_mask; // Store the starting bit mask
+			current_datum_decoded = 1; // Initialize the decoded value to 1
+
+			// Loop to count consecutive 1-bits in the encoded data
 			for(;*current_encoded_it & bit_mask; next_bit())
-				current_datum_decoded += 1;
+				current_datum_decoded += 1;  // Increment the decoded value for each consecutive 1-bit
 		}
 
 	public:
-		const uint64_t& operator*() const { return current_datum_decoded; }
-		const uint64_t* operator->() const { return &current_datum_decoded; }
+		// Overloaded operators and member functions for the iterator
+		const uint64_t& operator*() const { return current_datum_decoded; } // Dereferencing operator
+		const uint64_t* operator->() const { return &current_datum_decoded; } // Member access operator
 
 		iterator& operator++()
 		{
-			next_bit();
+			next_bit(); // Move to the next bit
 			if(current_encoded_it != end_encoded_it)
-				parse_current();
+				parse_current(); // If not at the end, parse the current bits
 
 			return *this;
 		}
 
-		const EncondedDataIterator& get_raw_iterator() const {return current_encoded_it;}
-		unsigned get_bit_offset() const {return std::countr_zero(current_datum_start_bit_mask);}
+		const EncondedDataIterator& get_raw_iterator() const {return current_encoded_it;} // Access raw iterator
+		unsigned get_bit_offset() const {return std::countr_zero(current_datum_start_bit_mask);} // Get bit offset
 
 		bool operator==(const iterator& b) const
 		{
-			return bit_mask == b.bit_mask and current_encoded_it == b.current_encoded_it;
+			return bit_mask == b.bit_mask and current_encoded_it == b.current_encoded_it; // Comparison operator
 		}
 
-		bool operator!=(const iterator& b) const { return not operator==(b); }
+		bool operator!=(const iterator& b) const { return not operator==(b); } // Inequality operator
 
-		friend UnaryDecoder;
+		friend UnaryDecoder; // Allowing UnaryDecoder class to access private members
 	private:
 	};
-
+	/**
+	 * Returns an iterator object pointing to the beginning of the encoded data
+	 * after initializing it with 'begin_it', 'end_it', and 'start_off' values.
+	 *
+	 * @return An iterator object pointing to the beginning of the encoded data.
+	 */
 	iterator begin() const
 	{
-		auto begin = iterator(begin_it, end_it, start_off);
-		begin.parse_current();
-		return begin;
+		auto begin = iterator(begin_it, end_it, start_off); // Initialize iterator 'begin' with 'begin_it', 'end_it', and 'start_off'
+		begin.parse_current(); // Parse the current bits to decode the initial value
+		return begin; // Return the initialized iterator
 	}
 
+	/**
+	 * Returns an iterator object pointing to the end of the encoded data.
+	 * The iterator is initialized with 'end_it' (indicating the end of the encoded data).
+	 *
+	 * @return An iterator object pointing to the end of the encoded data.
+	 */
 	iterator end() const
 	{
 		return iterator(end_it, end_it);
 	}
 
+	/**
+	 * Returns an iterator object pointing to a specific position within the encoded data.
+	 * The iterator is initialized with an offset 'off' from 'begin_it' and 'end_it' iterators.
+	 * The 'bit_off' parameter indicates the starting bit offset (defaulted to 0 if not provided).
+	 *
+	 * @param off The offset indicating the position within the encoded data.
+	 * @param bit_off Bit offset value indicating the starting bit (default: 0).
+	 * @return An iterator object pointing to the specified position within the encoded data.
+	 */
 	iterator at(size_t off, unsigned bit_off = 0) const
 	{
 		auto it = iterator(begin_it + off, end_it, bit_off);
@@ -163,7 +214,14 @@ public:
 		uint64_t buffer = 0;
 		uint8_t out_buffer = 0;
 		bool eos = false;
-
+		/**
+		 * Initializes the iterator object with the current iterator, end iterator, and an end-of-sequence flag.
+		 * If 'eos' is false (indicating not the end of the sequence), it builds the output buffer.
+		 *
+		 * @param current_it Iterator pointing to the current position in the raw data
+		 * @param end_raw_it Iterator pointing to the end of the raw data
+		 * @param eos Flag indicating if it's the end of the sequence (default: false)
+		 */
 		explicit iterator(RawDataIterator current_it, RawDataIterator end_raw_it, bool eos = false):
 				current_raw_it(current_it), end_raw_it(end_raw_it), eos(eos)
 		{
@@ -171,9 +229,23 @@ public:
 				build_out_buffer();
 		}
 
+		/**
+		 * Function build_out_buffer():
+		 * Constructs the output buffer by processing the raw data buffer.
+		 * It iterates through the bits of the buffer, populating the 'out_buffer' with unary-encoded values.
+		 *
+		 * The function operates as follows:
+		 * - Initializes 'out_buffer' to 0.
+		 * - Iterates through each bit using 'bit_mask'.
+		 * - Checks if 'buffer' is empty.
+		 *   - If empty, retrieves a new value from 'current_raw_it' and increments the iterator.
+		 * - Constructs 'out_buffer' by applying unary encoding logic:
+		 *   - Uses a bitwise OR operation to set specific bits based on the value of 'buffer'.
+		 * - Decrements 'buffer' by 1 for each bit processed.
+		 */
 		void build_out_buffer()
 		{
-			out_buffer = 0;
+			out_buffer = 0; // Initialize the output buffer to 0
 			for(uint8_t bit_mask = 0b00000001; bit_mask != 0; bit_mask <<= 1)
 			{
 				if (!buffer)
@@ -181,13 +253,13 @@ public:
 					if (current_raw_it == end_raw_it)
 						break;
 
-					buffer =  *current_raw_it;
-					++ current_raw_it;
+					buffer =  *current_raw_it;  // Fetch a new value from 'current_raw_it'
+					++ current_raw_it;			// Move to the next element in the raw data
 				}
-
+				// Constructs 'out_buffer' by setting specific bits based on the value of 'buffer'
 				out_buffer |= bit_mask & (buffer > 1 ? 0xff : 0);
 
-				buffer -= 1;
+				buffer -= 1; // Decrease 'buffer' value by 1
 			}
 		}
 
@@ -196,15 +268,43 @@ public:
 		const uint8_t& operator*() const { return out_buffer; }
 		const uint8_t* operator->() const { return &out_buffer; }
 
+		/**
+		 * Prefix increment operator 'operator++()':
+		 * Moves the iterator to the next position in the raw data sequence.
+		 *
+		 * The function performs the following operations:
+		 * - Checks if the iterator has reached the end of the sequence.
+		 *   - If true, sets the 'eos' flag to true indicating the end of the sequence.
+		 * - Calls 'build_out_buffer()' to construct the output buffer for the next position.
+		 * - Increments the iterator to the next position in the raw data sequence.
+		 *
+		 * @return Reference to the updated iterator object.
+		 */
 		iterator& operator++()
 		{
 			if(current_raw_it == end_raw_it)
-				eos = true;
+				eos = true; // Set 'eos' flag to true if the iterator reaches the end of the sequence
 
-			build_out_buffer();
-			return *this;
+			build_out_buffer(); // Construct the output buffer for the next position
+			return *this; // Return reference to the updated iterator object
 		}
 
+		/**
+		 * Comparison operator 'operator==':
+		 * Checks for equality between two iterator objects.
+		 *
+		 * The function performs the following equality check:
+		 * - Compares the 'eos' flags of both iterators.
+		 *   - If equal, returns true indicating equality.
+		 * - If 'eos' flags are different:
+		 *   - Compares the current positions of the iterators.
+		 *   - Ensures that the iterators have not reached the end position or are at the last element.
+		 *   - Returns true if both conditions are met; otherwise, returns false.
+		 *
+		 * @param a The first iterator for comparison
+		 * @param b The second iterator for comparison
+		 * @return True if the iterators are equal; otherwise, false
+		 */
 		friend bool operator== (const iterator& a, const iterator& b)
 		{
 			// This generates simpler asm
--- src/codes/variable_blocks.hpp
+++ src/codes/variable_blocks.hpp
@@ -29,6 +29,16 @@ public:
 		explicit iterator(EncondedDataIterator p, EncondedDataIterator s):
 			current_encoded_it(p), end_encoded_it(s) {}
 
+		/**
+		* Function 'parse_and_align()':
+		* Parses and aligns encoded data to retrieve the decoded datum.
+		*
+		* The function performs the following operations:
+		* - Initializes 'current_datum_decoded' to 0.
+		* - Saves the next input offset in 'next_input'.
+		* - Appends the 7 useful bits to 'current_datum_decoded' until reaching the end byte or the end of the iterator.
+		* - Sets 'next_encoded_it' to the next input offset after parsing and aligning.
+		*/
 		void parse_and_align()
 		{
 			current_datum_decoded = 0;
@@ -51,6 +61,16 @@ public:
 		const uint64_t& operator*() const {return current_datum_decoded;}
 		const uint64_t* operator->() const {return &current_datum_decoded;}
 
+		/**
+		 * Prefix increment operator 'operator++()':
+		 * Moves the iterator to the next position in the encoded data sequence and parses the next datum.
+		 *
+		 * The function performs the following operations:
+		 * - Assigns 'next_encoded_it' to 'current_encoded_it'.
+		 * - Checks if the current iterator position is not the end position.
+		 *   - If true, invokes 'parse_and_align()' to parse the next datum.
+		 * - Returns a reference to the updated iterator.
+		 */
 		iterator& operator++()
 		{
 			current_encoded_it = next_encoded_it;
@@ -70,18 +90,44 @@ public:
 	VariableBlocksDecoder(EncondedDataIterator start, const EncondedDataIterator& end) :
 			encoded_begin(start), encoded_end(end) {}
 
+	/**
+	 * Function 'begin()':
+	 * Initializes an iterator pointing to the beginning of the encoded data sequence and aligns the parsed datum.
+	 *
+	 * The function performs the following operations:
+	 * - Creates an iterator 'begin' initialized with 'encoded_begin' and 'encoded_end'.
+	 * - Invokes 'parse_and_align()' to align and parse the datum at the beginning position.
+	 * - Returns the initialized iterator pointing to the start of the encoded data sequence.
+	 */
 	iterator begin() const
 	{
 		auto begin = iterator(encoded_begin, encoded_end);
 		begin.parse_and_align();
 		return begin;
 	}
-
+	/**
+	 * Function 'end()':
+	 * Returns an iterator pointing to the end position of the encoded data sequence.
+	 *
+	 * The function performs the following operations:
+	 * - Returns an iterator initialized with 'encoded_end' as both start and end positions.
+	 *   This effectively represents the end of the encoded data sequence.
+	 * - Provides an iterator pointing to the position after the last element.
+	 */
 	iterator end() const
 	{
 		return iterator(encoded_end, encoded_end);
 	}
-
+	/**
+ * Function 'at()':
+ * Returns an iterator pointing to the specified offset position in the encoded data sequence, aligned with parsed data.
+ *
+ * The function performs the following operations:
+ * - Calculates the iterator 'begin' by adding the specified 'off' offset to 'encoded_begin'.
+ * - Creates an iterator 'begin' initialized with the calculated position and 'encoded_end'.
+ * - Invokes 'parse_and_align()' to align and parse the datum at the specified offset position.
+ * - Returns the initialized iterator pointing to the specified offset position in the sequence.
+ */
 	iterator at(size_t off) const
 	{
 		auto begin = iterator(encoded_begin + off, encoded_end);
@@ -97,6 +143,18 @@ class VariableBlocksEncoder
 	RawDataIterator raw_end;
 
 public:
+	/**
+	 * Static assertion ensuring that the value type of the provided RawDataIterator is integral.
+	 *
+	 * The static_assert performs compile-time validation to ensure that the value type associated with the RawDataIterator
+	 * is an integral type (e.g., int, char, long, etc.).
+	 *
+	 * Explanation:
+	 * - 'std::iterator_traits' provides a way to obtain information about iterators.
+	 * - 'std::iterator_traits<RawDataIterator>::value_type' retrieves the value type associated with 'RawDataIterator'.
+	 * - 'std::is_integral_v' is a type trait that checks if a given type is integral (i.e., a built-in integer type).
+	 * - The 'static_assert' generates a compile-time error if the value type is not integral.
+	 */
 	static_assert(std::is_integral_v<typename std::iterator_traits<RawDataIterator>::value_type>);
 
 	VariableBlocksEncoder(RawDataIterator begin, RawDataIterator end):
@@ -118,6 +176,17 @@ public:
 
 		explicit iterator(RawDataIterator current_it): current_it(current_it) {}
 	public:
+		/**
+		 * Dereference operator:
+		 * Retrieves the current value pointed to by the iterator.
+		 *
+		 * The function performs the following operations:
+		 * - Checks if 'working' is false, indicating the need to update 'buffer' from 'current_it'.
+		 * - Determines whether the number is representable in 7 bits:
+		 *   - If the number is not representable in 7 bits, sets the most significant bit to 1.
+		 *   - If the number is representable, sets the MSB to 0.
+		 * @Returns the processed value that aligns and represents the current value pointed to by the iterator.
+		 */
 		uint8_t operator*()
 		{
 			if(not working)
@@ -128,6 +197,21 @@ public:
 			return (buffer > 0b01111111ul ? 0b10000000 : 0) | (buffer & 0b01111111);
 		}
 
+		/**
+		 * Prefix increment operator:
+		 * Advances the iterator to the next position and updates the buffer for the next value.
+		 *
+		 * The function performs the following operations:
+		 * - Shifts the 'buffer' right by 7 bits, preparing for the next value.
+		 * - Checks if 'buffer' still contains a value:
+		 *   - If 'buffer' is not empty, sets 'working' to true, indicating that the iterator is still processing.
+		 *     Then, returns the updated iterator.
+		 * - If 'buffer' is empty:
+		 *   - Increments 'current_it' to move to the next element.
+		 *   - Resets 'working' to false, indicating that a new buffer needs to be loaded.
+		 *   - Loads the next value into 'buffer'.
+		 *  @Returns a reference to the updated iterator.
+		 */
 		iterator& operator++()
 		{
 			buffer = buffer >> 7;
@@ -161,8 +245,18 @@ struct VariableBytes
 	unsigned short used_bytes = 0;
 
 	/**
-	 * Takes a number and compresses it using the Variable Bytes method
-	 * The compressed number is put in the variable `bytes[]`
+	 * Constructor for the VariableBytes struct, using Variable Byte Encoding to compress a number.
+	 *
+	 * The constructor performs Variable Byte Encoding on the provided 'number' and stores the compressed bytes.
+	 * - 'number': The input number to be compressed using Variable Byte Encoding. Default value is 0.
+	 *
+	 * The function performs the following operations:
+	 * - Uses a loop to compress the 'number' into a series of bytes using Variable Byte Encoding.
+	 * - Iterates until 'number' becomes 0:
+	 *    - Extracts 7 bits from the 'number', stores them in a byte with the MSB set to 1 (0b10000000).
+	 *    - Appends the created byte to the 'bytes' array and increments 'used_bytes'.
+	 * - Adds a terminator byte (0b01111111) to mark the end of the compressed bytes if 'used_bytes' is non-zero.
+	 *   - If 'used_bytes' is 0, sets 'used_bytes' to 1.
 	 */
 	explicit inline VariableBytes(uint64_t number = 0)
 	{
@@ -177,8 +271,18 @@ struct VariableBytes
 	}
 
 	/**
-	 * Parses a variable-length number and returns it uncompressed
-	 * @returns the parsed bytes and the number of bytes read
+	 * Parses a sequence of bytes using Variable Byte Decoding to reconstruct the original number.
+	 *
+	 * The function performs Variable Byte Decoding on the input 'bytes' to reconstruct the original number.
+	 * - 'bytes': A pointer to the sequence of bytes to be parsed.
+	 *
+	 * The function iterates through the 'bytes' array:
+	 * - Iterates up to a maximum of 10 times or until encountering the end marker in the bytes.
+	 * - For each byte in the sequence:
+	 *    - Retrieves the 7 bits from the byte (excluding the most significant bit) and adds it to 'number'.
+	 *    - Moves the bits to the correct position in 'number' using bitwise left shifts (<<).
+	 *    - Checks the most significant bit to determine if there are more bytes to read ('more' flag).
+	 *  @Returns a pair containing the reconstructed 'number' and the count of bytes read (i).
 	 */
 	static inline std::pair<uint64_t, unsigned> parse(uint8_t *bytes)
 	{
